6daa9832376ed9cc2bca3beab169fa0df7637929
George <96864675+gogi1704@users.noreply.github.com>
2022-08-18 13:39:13 +0300

    pushService complete

--- src/main/kotlin/notification_pusher/Main.kt
+++ src/main/kotlin/notification_pusher/Main.kt
@@ -7,8 +7,9 @@ import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import java.io.FileInputStream
 
+const val TOKEN = "cFJdjC1UT16W34KYRzyCeF:APA91bGtQ9ITEz6CodE_tpQSqIXPyaGlxiSoyK5rtRKh-Vzuy_MvUsuf0RWy4wTt_x8StxIIP-q9NVuatGjrXFQnYNVwNOJpamTAEpUBAyk5jPuje4An7IAMgxre7nVHbO3X2N45iwe8"
+
 fun main() {
-    val token = "chKbXqDvSjyAj5HJZG8cQp:APA91bFrfxXIRxJLl5viFPF4ZAPwcDb9XMDN-GutYhiaQFEJIZJ3bnuUiaktMx0cLShxtS0hT3xGw-eBLI1-StDKHelm76IOBIR_zPmWnJkpsD5JmfQEOJzGJZNL1W0RvSs0D7jkvwBZ"
     val options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
         .build()
@@ -16,17 +17,21 @@ fun main() {
     FirebaseApp.initializeApp(options)
 
     val message = Message.builder()
-        .putData("action", "like")
-        .putData(
-            "content", """
-            "userId": 1 , 
-            "userName": "Vasiliy",
-            "postId": 2 ,
-            "postAuthor": "Netology"
-        """.trimIndent()
-        )
-        .setToken(token)
+        .putData("action" , "NEW_POST")
+        .putData("content" , """{
+          "id": 2,
+          "title": "New post",
+          "date": "22.12.22",
+          "content": "By default, the notification's text content is truncated to fit one line. If you want your notification to be longer, you can enable an expandable notification by adding a style template with setStyle(). For example, the following code creates a larger text area:",
+          "likes": 0,
+          "shares" : 0 ,
+          "shows": 0,
+          "isLiked": "true"
+        }
+            
+        """.trimIndent())
+        .setToken(TOKEN)
         .build()
-
     FirebaseMessaging.getInstance().send(message)
+
 }
